74602e0ac6088eb468811c4cf4895c0249d5a135
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-02-18 15:40:38 +0000

    feat: added update decorator function

--- codequest.py
+++ codequest.py
@@ -3,19 +3,26 @@ import sys
 
 
 class Game:
-    def __init__(self, name="window", width=500, height=500, background_color = (255, 255, 255), fps = 60):
+    def __init__(
+        self,
+        name="window",
+        width=500,
+        height=500,
+        background_color=(255, 255, 255),
+        fps=60,
+    ):
         pygame.init()
-        #Screen
+        # Screen
         self.screen = pygame.display.set_mode((width, height))
         self.background_color = background_color
         pygame.display.set_caption(name)
-        
-        #Settings
+
+        # Settings
         self.clock = pygame.time.Clock()
         self.events = pygame.event.get()
         self.fps = fps
-        
-        self.run()
+
+        self.update_functions = []
 
     def check_events(self):
         self.clock.tick()
@@ -24,10 +31,19 @@ class Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            print(event.dict)
+            # print(event.dict)
+
+    def add_update_function(self, func):
+        self.update_functions.append(func)
+
+    def on_update(self, func):
+        self.add_update_function(func)
+        return func
 
     def update(self):
         self.screen.fill(self.background_color)
+        for func in self.update_functions:
+            func(self)
         pygame.display.update()
 
     def run(self):
